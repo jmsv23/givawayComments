f7a96956353a49452e3a98f206799b4b9a3549d2
jmsv23 <jmsv23@gmail.com>
2019-04-05 14:43:07 -0600

    load comments

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ const env = require('node-env-file')
 const fetch = require('node-fetch')
 
 env(__dirname + '/.env')
-
 const url = 'https://www.googleapis.com/youtube/v3/commentThreads'
-const apiKey = process.env.API_KEY || ''
-const format = 'plainText'
-const part = 'snippet'
-const videoId = 'u_vXGrrzIiQ'
 
-fetch(`${url}?key=${apiKey}&textFormat=${format}&part=${part}&videoId=${videoId}`)
-.then((response) => {
-  return response.json()
-})
-.then((data) => {
-  return console.log(data)
-})
-.catch((error) => {
-  console.log(error)
-})
+const getComments = (url) => {
+  const apiKey = process.env.API_KEY || ''
+  const format = 'plainText'
+  const part = 'snippet'
+  const videoId = 'u_vXGrrzIiQ'
+  return fetch(`${url}?key=${apiKey}&textFormat=${format}&part=${part}&videoId=${videoId}`)
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    return data;
+  })
+  .catch((error) => {
+    console.log(error)
+    return null
+  })
+}
+
+getComments(url)
+.then(data => console.log(data))
