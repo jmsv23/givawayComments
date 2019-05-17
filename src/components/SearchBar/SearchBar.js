67143f402c8dd512301ff10e92ef225042c2279c
jmsv23 <jmsv23@gmail.com>
2019-05-17 16:55:38 -0500

    inprove the display of information

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -6,11 +6,13 @@ export default class SearchBar extends Component {
   static propTypes = {
     placeholder: PropTypes.string,
     onClick: PropTypes.func,
+    onChange: PropTypes.func,
   }
 
   static defaultProps = {
     placeholder: 'Search...',
     onClick: () => {},
+    onChange: () => {},
   }
 
   constructor(props) {
@@ -26,19 +28,20 @@ export default class SearchBar extends Component {
     } = this.state
     this.props.onClick(input)
   }
+
+  onChange = (e) => {
+    this.setState({ input: e.target.value })
+    this.props.onChange(e.target.value)
+  }
+
   render() {
     const {
       placeholder,
     } = this.props
     return (
       <Input
-       onChange={(e) => { this.setState({ input: e.target.value })}}
+       onChange={this.onChange}
        placeholder={placeholder}
-       action={{
-         icon: 'search',
-         color: 'teal',
-         onClick: this.onClick,
-       }}
       />
     )
   }
